d2c8921e1acae8e6d7b8327f0e9e81dba1e74672
root <root@DESKTOP-76JROMF.localdomain>
2020-09-29 19:48:54 -0400

    adding new code

--- euler/e4.py
+++ euler/e4.py
@@ -18,5 +18,5 @@ def longestPalindrome(m=100, n=100):
     return max([i * j for i in range(m, 1000) for j in range(n, 1000) if str(i * j) == str(i * j)[::-1]])
 
 
-if __name__ == "__main__":::
+if __name__ == "__main__":
     print(longestPalindrome())
