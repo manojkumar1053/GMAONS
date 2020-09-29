da536f78c14fed56342845edb9e0dbcd4c46347b
root <root@DESKTOP-76JROMF.localdomain>
2020-09-29 19:12:27 -0400

    e4 list comprehension

--- euler/e4.py
+++ euler/e4.py
@@ -3,15 +3,19 @@
 Find the largest palindrome made from the product of two 3-digit numbers."""
 
 
+#
+# def longestPalindrome(m=100, n=100):
+#     palindrome = []
+#     for i in range(m, 1000, 1):
+#         for j in range(n, 1000, 1):
+#             x = i * j
+#             lx = list(str(i * j))
+#             if lx == lx[::-1]:
+#                 palindrome.append(x)
+#     return max(palindrome)
+
 def longestPalindrome(m=100, n=100):
-    palindrome = []
-    for i in range(m, 1000, 1):
-        for j in range(n, 1000, 1):
-            x = i * j
-            lx = list(str(i * j))
-            if lx == lx[::-1]:
-                palindrome.append(x)
-    return max(palindrome)
+    return max([i * j for i in range(m, 1000) for j in range(n, 1000) if str(i * j) == str(i * j)[::-1]])
 
 
 if __name__ == "__main__":
