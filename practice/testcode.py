fb0ef47f4f7a4930657b12d3269e3032736e5135
root <root@DESKTOP-76JROMF.localdomain>
2020-10-01 17:22:54 -0400

    adding new code

---
+++ practice/testcode.py
@@ -0,0 +1,15 @@
+capacity = 10
+items = 4
+
+kpc = [[0 for x in range(0, capacity + 1)]]
+kit = [[0] for y in range(0, items + 1)]
+kittest = [0 for y in range(0, items + 1)]
+print(kittest)
+
+kp_total = [[0 for x in range(0, capacity + 1)] for y in range(0, items + 1)]
+
+print("kpc", kpc)
+print("kit", kit)
+print(kp_total)
+print(items[2][2])
+
