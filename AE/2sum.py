4c6dc7cda6de34c494647a8c115d9e2dfbe8ffd5
root <root@DESKTOP-76JROMF.localdomain>
2020-10-02 23:38:09 -0400

    adding new code

---
+++ AE/2sum.py
@@ -0,0 +1,10 @@
+#  time O(n) | space O(n)
+
+def twoSum(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
