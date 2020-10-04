8eb949ec7ecc2293430675fa9665f91513f851f8
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-03 23:32:02 -0400

    adding new code

--- AE/yongestCommonAncestor.py
+++ AE/yongestCommonAncestor.py
@@ -1,1 +1,7 @@
-print("ons")
+def getYongestCommonAncestor(topAncestor, descendantOne, descendantTwo):
+    depthOne = getDescendantDepth(descendantOne, topAncestor)
+    depthTwo = getDescendantDepth(descendantTwo, topAncestor)
+
+
+def getDescendantDepth(descendant, ancestor):
+    depth = 0
