6c3225fc5fc181a17ac8cb4d2a1a802d0920c622
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-03 21:52:21 -0400

    test

--- AE/numberOfWaysMakeChange.py
+++ AE/numberOfWaysMakeChange.py
@@ -1,3 +1,9 @@
 def numberOfWaysToMakeChange(n, denoms):
     # Write your code here.
-    pass
+    ways = [0 for amount in range(n + 1)]
+    ways[0] = 1
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if denom <= amount:
+                ways[amount] += [amount - denom]
+    return ways[n]
