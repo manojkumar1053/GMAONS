8998bd4040717c66ee991064a5060b55302412c3
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-06 00:41:44 -0400

    group Anagram

--- string/groupAnagram.py
+++ string/groupAnagram.py
@@ -1,2 +1,31 @@
 def groupAnagrams(words):
-    pass
+    sortedWords = ["".join(sorted(w)) for w in words]
+    indices = [i for i in range(len(words))]
+    indices.sort(key=lambda x: sortedWords[x])
+
+    result = []
+    currentAnagramGroup = []
+    currentAnagram = sortedWords[indices[0]]
+    for index in indices:
+        word = words[index]
+        sortedWord = sortedWords[index]
+        if sortedWords == currentAnagram:
+            currentAnagramGroup.append(word)
+            continue
+        result.append(currentAnagramGroup)
+        currentAnagramGroup = [word]
+        currentAnagram = sortedWord
+    result.append(currentAnagramGroup)
+
+    return result
+
+
+def gAnagram(words):
+    anagrams = {}
+    for word in words:
+        sortedWord = "".join(sorted(word))
+        if sortedWord in anagrams:
+            anagrams[sortedWord].append(word)
+        else:
+            anagrams[sortedWord] = [word]
+    return list(anagrams.values())
