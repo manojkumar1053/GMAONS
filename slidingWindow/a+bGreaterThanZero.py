a4ede01dec3832315ef1ec6cae8a0730c7e0eef8
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-06 22:15:41 -0400

    smallest contiguous subarray whose sum is greater than or equal to ‘S’

--- slidingWindow/a+bGreaterThanZero.py
+++ slidingWindow/a+bGreaterThanZero.py
@@ -1,4 +1,15 @@
+"""
+Given an array of positive numbers
+and a positive number ‘S’,
+find the length of the smallest
+contiguous subarray whose sum is greater than or equal to ‘S’.
+Return 0, if no such subarray exists
+"""
+
+
 def smallest_subarray_with_given_sum(s, arr):
+    if len(arr) < 1:
+        return 0
     windowMin = float("inf")
     windowSum = 0.0
     windowStart = 0
