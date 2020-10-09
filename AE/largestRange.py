3d8bdf14c563a4f05963b8b91b2154c5f061d04b
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-09 18:25:37 -0400

    largestRange

--- AE/largestRange.py
+++ AE/largestRange.py
@@ -1,3 +1,25 @@
 def largestRange(array):
-
-    pass
+    bestRange = []
+    longestLength = 0
+    nums = {}
+    for num in array:
+        nums[num] = True
+    for num in array:
+        if not nums[num]:
+            continue
+        nums[num] = False
+        currentLength = 1
+        left = num - 1
+        right = num + 1
+        while left in nums:
+            nums[num] = False
+            currentLength += 1
+            left -= 1
+        while right in nums:
+            nums[num] = False
+            currentLength += 1
+            right += 1
+        if currentLength > longestLength:
+            longestLength = currentLength
+            bestRange = [left + 1, right - 1]
+    return bestRange
