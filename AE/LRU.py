20f7ad0d4ffb7f2e5fa4c3de1dde326e9a9d35bd
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-12 23:05:40 -0400

    LRu Code Chnages

--- AE/LRU.py
+++ AE/LRU.py
@@ -13,3 +13,36 @@ class LRUCache:
     def getMostRecentKey(self):
         # Write your code here.
         pass
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHeadTo(self,node):
+        if self.head == node:
+            return
+        elif self.head is None:
+            self.head = node
+            self.tail = node
+        elif self.head == self.tail:
+            self.tail.prev =node
+            self.head = node
+            self.head.next = self.tail
+
+
+class DoublyLinkedListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+    def removeBindings(self):
+        if self.prev is not None:
+            self.prev.next = self.next
+        if self.next is not None:
+            self.next.prev = self.prev
+        self.next = None
+        self.prev = None
