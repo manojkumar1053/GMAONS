e427bbcec75c59f4843639840e225012908bc3e1
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-12 21:54:16 -0400

    LRU Cache

---
+++ AE/LRU.py
@@ -0,0 +1,15 @@
+class LRUCache:
+    def __init__(self, maxSize):
+        self.maxSize = maxSize or 1
+
+    def insertKeyValuePair(self, key, value):
+        # Write your code here.
+        pass
+
+    def getValueFromKey(self, key):
+        # Write your code here.
+        pass
+
+    def getMostRecentKey(self):
+        # Write your code here.
+        pass
