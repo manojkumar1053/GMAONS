0f185c24d8d712aac4848b0af8fd7b9223b9606c
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-13 23:56:07 -0400

    LL methods

--- ll/dllConstruction.py
+++ ll/dllConstruction.py
@@ -13,20 +13,40 @@ class DoublyLinkedList:
         self.tail = None
 
     def setHead(self, node):
-        # Write your code here.
-        pass
+        if self.head is None:
+            self.head, self.tail = None, None
+            return
+        self.insertBefore(self.head, node)
 
     def setTail(self, node):
-        # Write your code here.
-        pass
+        if self.tail is None:
+            self.setHead(node)
+            return
+        self.insertBefore(self.tail, node)
 
     def insertBefore(self, node, nodeToInsert):
-        # Write your code here.
-        pass
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node.prev
+        nodeToInsert.next = node
+        if node.prev is None:
+            self.head = nodeToInsert
+        else:
+            node.prev.next = nodeToInsert
+        node.prev = nodeToInsert
 
     def insertAfter(self, node, nodeToInsert):
-        # Write your code here.
-        pass
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node
+        nodeToInsert.next = node.next
+        if node.next == None:
+            self.tail = nodeToInsert
+        else:
+            node.next.prev = nodeToInsert
+        node.next = nodeToInsert
 
     def insertAtPosition(self, position, nodeToInsert):
         # Write your code here.
@@ -34,12 +54,29 @@ class DoublyLinkedList:
 
     def removeNodesWithValue(self, value):
         # Write your code here.
-        pass
+        node = self.head
+        while node is not None:
+            nodeToRemove = node
+            node = node.next
+            if node.value == value:
+                self.remove(node)
 
     def remove(self, node):
-        # Write your code here.
-        pass
+        if node == self.head:
+            self.head = self.head.next
+        if node == self.tail:
+            self.tail = self.tail.prev
+        self.removeNodeBindings(node)
+
+    def removeNodeBindings(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        node.prev, node.next = None, None
 
     def containsNodeWithValue(self, value):
-        # Write your code here.
-        pass
+        node = self.head
+        while node is not None and node.value != value:
+            node = node.next
+        return node is not None
