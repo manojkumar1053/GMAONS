60c10bfb111cac9033bbab048ff99b45fe8cf80f
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-18 18:42:56 -0400

    .\heap\minHeap.py

---
+++ heap/minHeap.py
@@ -0,0 +1,42 @@
+class MinHeap:
+    def __init__(self, array):
+        # Do not edit the line below.
+        self.heap = self.buildHeap(array)
+
+    def buildHeap(self, array):
+        # Write your code here.
+        pass
+
+    def siftDown(self, currentIdx, endIdx, heap):
+        # Write your code here.
+        childOneIdx = currentIdx * 2 + 1
+        while childOneIdx <= endIdx:
+            childTwoIdx = currentIdx * 2 + 2 if childOneIdx * 2 + 2 <= endIdx else -1
+            if childTwoIdx != -1 and heap[childTwoIdx] < heap[childOneIdx]:
+                pass
+
+
+    def siftUp(self, currentIdx, heap):
+        # Write your code here.
+        parentIdx = (currentIdx - 1) // 2
+        while currentIdx > 0 and heap[currentIdx] < heap[parentIdx]:
+            self.swap(currentIdx, parentIdx, heap)
+            currentIdx = parentIdx
+            parentIdx = (currentIdx - 1) // 2
+
+    def peek(self):
+        # Write your code here.
+        pass
+
+    def remove(self):
+        self.swap(0, len(self.heap) - 1, self.heap)
+        valueToRempve = self.heap.pop()
+        self.siftDown(0, len(self.heap) - 1, self.heap)
+
+    def insert(self, value):
+        # Write your code here.
+        self.heap.append(value)
+        self.siftUp(len(self.heap) - 1, self.heap)
+
+    def swap(self, i, j, heap):
+        heap[i], heap[j] = heap[j], heap[i]
