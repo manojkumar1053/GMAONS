6a7c887c103986c6186780c06e02f749e7efb159
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-20 19:34:53 -0400

    .\seraching\binarySearch.py

--- seraching/binarySearch.py
+++ seraching/binarySearch.py
@@ -13,3 +13,20 @@ def binarySearchHelper(array, target, left, right):
         return binarySearchHelper(array, target, left, middle - 1)
     else:
         return binarySearchHelper(array, target, middle + 1, right)
+
+
+def binarySearch1(array, target):
+    return binarySearchHelper(array, target, 0, len(array) - 1)
+
+
+def binarySearchHelper1(array, target, left, right):
+    while left < right:
+        middle = (left + right) // 2
+        targetMatch = array[middle]
+        if target == targetMatch:
+            return middle
+        elif target < targetMatch:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
