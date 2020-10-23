45f3895ed55d703ba573a930a4ae9fe8abe8e44d
manojkumar1053 <manojkumar1053@gmail.com>
2020-10-22 20:17:17 -0400

    .\heap\medianStream.py

---
+++ heap/medianStream.py
@@ -0,0 +1,14 @@
+# Do not edit the class below except for
+# the insert method. Feel free to add new
+# properties and methods to the class.
+class ContinuousMedianHandler:
+    def __init__(self):
+        # Write your code here.
+        self.median = None
+
+    def insert(self, number):
+        # Write your code here.
+        pass
+
+    def getMedian(self):
+        return self.median
